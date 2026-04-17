41e716234a9c3e46a6606e345fcf120d1114f2dc
omargriffin14 <61708895+omargriffin14@users.noreply.github.com>
2026-04-17 14:28:56 -0400

    Add outputs for EC2, S3, CloudFront, and Secrets Manager

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "backend_ec2_id" {
+  value = aws_instance.backend.id
+}
+
+output "backend_ec2_private_ip" {
+  value = aws_instance.backend.private_ip
+}
+
+output "frontend_s3_bucket" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.frontend.domain_name}"
+}
+
+output "db_secret_arn" {
+  value = aws_secretsmanager_secret.db_credentials.arn
+}
