401f3ff5a1c7d59bcd8452fca29b2001c6d4783e
omargriffin14 <61708895+omargriffin14@users.noreply.github.com>
2026-05-05 21:33:42 -0400

    Update EC2 output values in outputs.tf
    Replaced backend EC2 private IP output with public IP and removed NAT gateway output.

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@ output "backend_ec2_id" {
   value = aws_instance.backend.id
 }
 
-output "backend_ec2_private_ip" {
-  value = aws_instance.backend.private_ip
+output "backend_ec2_public_ip" {
+  value = aws_instance.backend.public_ip
 }
 
 output "frontend_s3_bucket" {
@@ -16,10 +16,6 @@ output "cloudfront_url" {
 
 output "db_secret_arn" {
   value = aws_secretsmanager_secret.db_credentials.arn
-}
-
-output "nat_gateway_id" {
-  value = aws_nat_gateway.main.id
 }
 
 output "uploads_bucket_name" {
